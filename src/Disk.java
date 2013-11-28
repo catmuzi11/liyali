9952135a13a4f18fe169dac0e1e53f040f698a98
liyali <cat424479018@sina.com>
2013-11-28 16:59:14 +0800

    11111

---
+++ src/Disk.java
@@ -0,0 +1,27 @@
+package com.example.odoffice.view;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class Disk extends LinearLayout{
+
+	public Disk(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Disk(Context context ,AttributeSet attr){
+		super(context, attr);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+		super.onLayout(changed, l, t, r, b);
+	}
+
+	
+	
+}
